eb13437f417d0227e4a77b5c6a36b5b915334a7d
ira8296 <ira8296@g.rit.edu>
2021-11-09 22:32:33 -0500

    Transferred project files

---
+++ IGM Seminar Project/SmashFest/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,142 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    enum Scenes { Title, Instructions, Menu, Game, GameOver, Win }
+    GameObject p1;
+    GameObject p2;
+    GameObject p3;
+    GameObject p4;
+    List<GameObject> players;
+    List<int> scores;
+    Vehicle script;
+    Scene active;
+    static Scenes current = Scenes.Title;
+
+    public static SceneManager manager;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        GameObject scene = GameObject.Find("SceneManager");
+        if (scene != null && scene != this.gameObject)
+        {
+            Destroy(scene);
+        }
+        //DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        active = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+        //reference = gameObject.GetComponent<GUIManager>();
+
+        if (active.name == "Title")
+        {
+            current = Scenes.Title;
+        }
+        else if(active.name == "Instructions")
+        {
+            current = Scenes.Instructions;
+        }
+        else if(active.name == "Menu")
+        {
+            current = Scenes.Menu;
+        }
+        else if (active.name == "Game")
+        {
+            current = Scenes.Game;
+        }
+        else if (active.name == "Game Over")
+        {
+            current = Scenes.GameOver;
+        }
+        else if (active.name == "Win")
+        {
+            current = Scenes.Win;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //reference = gameObject.GetComponent<GUIManager>();
+        if (current == Scenes.Title)
+        {
+            p1 = null;
+            p2 = null;
+            p3 = null;
+            p4 = null;
+            //reference.enabled = false;
+        }
+        else if (current == Scenes.Game)
+        {
+            //reference.enabled = false;
+            p1 = GameObject.Find("Vehicle_Blue");
+            p2 = GameObject.Find("Vehicle_Green");
+            p3 = GameObject.Find("Vehicle_Red");
+            p4 = GameObject.Find("Vehicle_Dark");
+
+            players.Add(p1);
+            players.Add(p2);
+            players.Add(p3);
+            players.Add(p4);
+
+            for(int n = 0; n < players.Count; n++)
+            {
+                if(players[n] != null)
+                {
+                    Vehicle player = players[n].GetComponent<Vehicle>();
+                    int score = player.score;
+                    scores.Add(score);
+
+                    if(player.Health <= 0)
+                    {
+                        players.Remove(players[n]);
+                        Destroy(players[n]);
+                    }
+                }
+            }
+
+            if(players.Count == 1)
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Win");
+                current = Scenes.Win;
+            }
+        }
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+        current = Scenes.Game;
+
+    }
+
+    public void Instructions()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Instructions");
+        current = Scenes.Instructions;
+    }
+
+    public void BackToMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+        current = Scenes.Menu;
+    }
+
+    public void BackToStart()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Title");
+        current = Scenes.Title;
+    }
+}
---
+++ IGM Seminar Project/SmashFest/Assets/Scripts/Vehicle.cs
@@ -0,0 +1,201 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Vehicle : MonoBehaviour
+{
+
+    public Vector3 position;
+    public Vector3 direction;
+    public Vector3 velocity;
+    public Vector3 acceleration;
+    public int health;
+    public int score;
+    enum Direction { Left, Right, Forward, Backward }
+
+    public float speed = 0.1f;
+    public float maxSpeed = 0.3f;
+
+    public float thrust = 0.0f;
+
+    float accelerationInput = 0;
+    float steeringInput = 0;
+
+    float rotationAngle = 0;
+
+    Rigidbody2D carRigidbody2D;
+
+    public int Health
+    {
+        get { return health; }
+        set { health = value; }
+    }
+    public int Score
+    {
+        get { return score; }
+    }
+    public Vector3 Vector
+    {
+        get { return direction; }
+    }
+    public Vector3 Local
+    {
+        get { return position; }
+        set { position = value; }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        thrust = 0.0f;
+        position = new Vector3(-15, 5, -1);
+        direction = new Vector3(0, 1);
+        velocity = new Vector3(0, 0);
+        acceleration = new Vector3(0, 0);
+        health = 100;
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.gameObject != null)
+        {
+            //Logic for accelerating and turning
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                Turn(Direction.Right);
+            }
+            /*if (Input.GetKeyUp(KeyCode.LeftArrow))
+            {
+                thrust = 0.0f;
+                velocity = Vector3.zero;
+            }*/
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                Turn(Direction.Left);
+            }
+            /*if (Input.GetKeyUp(KeyCode.RightArrow))
+            {
+                thrust = 0.0f;
+                velocity = Vector3.zero;
+            }*/
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Advance(Direction.Forward);
+                speed = 0.1f;
+                thrust = 0.1f;
+            }
+            if (Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                thrust = 0.0f;
+                velocity = Vector3.zero;
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                Advance(Direction.Backward);
+                speed = -0.1f;
+                thrust = -0.1f;
+            }
+            if (Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                thrust = 0.0f;
+                velocity = Vector3.zero;
+            }
+
+
+            //If health is at or below 0, then the player is eliminated
+            if (health <= 0)
+            {
+                Vehicle self = this.GetComponent<Vehicle>();
+                self = null;
+            }
+        }
+
+        acceleration = thrust * direction;
+        velocity += acceleration * Time.deltaTime;
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+        Debug.DrawLine(position, position + velocity);
+
+        position += velocity;
+
+        //SendInputToServer();
+
+        WrapAround();
+
+        SetTransform();
+    }
+
+    private void SendInputToServer()
+    {
+        bool[] _inputs = new bool[]
+        {
+            Input.GetKey(KeyCode.LeftArrow),
+            Input.GetKey(KeyCode.RightArrow),
+            Input.GetKeyDown(KeyCode.UpArrow),
+            Input.GetKeyDown(KeyCode.DownArrow),
+            Input.GetKeyUp(KeyCode.UpArrow),
+            Input.GetKeyUp(KeyCode.DownArrow),
+        };
+
+        ClientSend.PlayerMovement(_inputs);
+    }
+
+    //Changes the vehicle's direction
+    void Turn(Direction d)
+    {
+        if (d == Direction.Left)
+        {
+            direction = Quaternion.Euler(0, 0, -1f) * direction;
+        }
+        else if (d == Direction.Right)
+        {
+            direction = Quaternion.Euler(0, 0, 1f) * direction;
+        }
+    }
+
+    //Adjusts vehicle's speed as it drives in its current direction
+    void Advance(Direction d)
+    {
+        if (d == Direction.Forward)
+        {
+            thrust += 0.1f;
+            speed = speed + 0.1f;
+        }
+        else if (d == Direction.Backward)
+        {
+            thrust += 0.1f;
+            speed = speed + 0.1f;
+        }
+    }
+
+    //Ensures that none of the vehicles go off the screen
+    void WrapAround()
+    {
+        if (position.x > 20.03f)
+        {
+            position.x = -19.0f;
+        }
+        if (position.x < -19.93f)
+        {
+            position.x = 20.0f;
+        }
+        if (position.y > 10.15f)
+        {
+            position.y = -9.0f;
+        }
+        if (position.y < -9.83f)
+        {
+            position.y = 10.0f;
+        }
+    }
+
+    void SetTransform()
+    {
+        float angle = Mathf.Atan2(direction.x, direction.y);
+        angle *= Mathf.Rad2Deg - 90;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+        transform.position = position;
+    }
+}
